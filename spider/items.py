59021822291f1ce460e89cf437a771255ce87062
小武 <1059174412@qq.com>
2019-12-02 10:16:23 +0800

    更新item字段

--- spider/items.py
+++ spider/items.py
@@ -10,29 +10,5 @@ import scrapy
 from scrapy.item import Item, Field
 
 class SpiderItem(Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-
-    cinema = Field()
-    magementname = Field()
-    province = Field()
-    city = Field()
-    code = Field()
-    fp = Field()
-    ky = Field()
-    sj = Field()
-    yinmushu = Field()
-    urlcode = Field()
-
-    '''
-    cinema2 = Field()
-    magementname2 = Field()
-    province2 = Field()
-    city2 = Field()
-    code2 = Field()
-    fp2 = Field()
-    ky2 = Field()
-    sj2 = Field()
-    yinmushu2 = Field()
-    urlcode2 = Field()
-    '''
+    
+    title = Field()
